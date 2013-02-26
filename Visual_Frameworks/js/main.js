0511b60c23035510af931190eff90272f30c537c
Eugene Proctor <eproctor@fullsail.edu>
2013-02-26 00:05:43 -0500

    Final Working Files for Project 3

--- Visual_Frameworks/js/main.js
+++ Visual_Frameworks/js/main.js
@@ -1,4 +1,6 @@
-/* Name: Eugene Proctor :: Project 2 :: VFW 1302 */
+/* Name: Eugene Proctor :: Project 3 :: VFW 1302 */
+
+
 
 
 
@@ -7,82 +9,87 @@
 
 	var keyValue = document.getElementById("key");
 	keyValue.innerHTML=Math.random();
+
+
+
+
+
+/* ==========  Variables  ========== */
+
+	/*alert ("Create Variables...");*/
+
+	var today = document.getElementById("startdate");
+	var name = document.getElementById("fullName");
+	var eMail = document.getElementById("email");
+	var travelCity = document.getElementById("city");
+	var surveyCheckBoxes = document.getElementById("checkBoxField").survey; 		
+	var comment = document.getElementById("description");
+	var rating = document.getElementById("rating");
+	
 	
 
 
 
-/* ========== Storage Function ========== */
 
-var storeData = function() {
-
-	var today, name, eMail;
-	var travelAtlanta, travelFlorida,travelCharlotte;
-	var surveySocialmedia, surveyTelevision, surveyRadios, surveyFriend;
-	var comment, rating, keyEntry;
+/* ===============  Test Check Box Test ============ */
 
 
-	alert ("Local Storage created...");
+ var testCheckBox = function() {
+ 
+ 	for(i=0, j=surveyCheckBoxes.length; i<j; i++) {
+ 			
+ 		if (surveyCheckBoxes[i].checked) {
+ 			console.log(surveyCheckBoxes[i].value);
+ 			localStorage.setItem("Survey Question", surveyCheckBoxes[i].value);
+ 			
+
+ 			
+ 		}  
+ 	}
+
+ };
+
+
+
+
+ /* ======  Save Data to Local Storage  ====== */
+
+ 
+var saveData = function() {
+
+	localStorage.setItem("Unique key", keyValue.innerHTML);
+	localStorage.setItem("Todays Date", startdate.value);
+	localStorage.setItem("Full Name", fullName.value); 
+	localStorage.setItem("Email Address", email.value);
+	localStorage.setItem("Travel City", city.value);
+	localStorage.setItem("Leave a Comment", description.value);
+	localStorage.setItem("Rate your Comfort Level", rating.value);
+
+	testCheckBox();
+
 	
-	localStorage.keyEntry = keyValue.innerHTML; 
-	localStorage.today = startdate.value;
-	localStorage.name = fullName.value;
-	localStorage.eMail = email.value;
-
-	localStorage.travelCity = city.value;
-	
-	localStorage.surveySocialmedia = socialmedia.checked;
-	localStorage.surveyTelevision = television.checked;
-	localStorage.surveyRadios = radios.checked;
-	localStorage.surveyFriend = friend.checked;
-	
-	localStorage.comment = description.value;
-	localStorage.comfort = rating;
-
-
 };
 
 
-/* ========== Display Data  ========== */
 
-var displayData = function() {
-			
-				/*	alert ("hello"); */
-				var newUlTag = document.createElement("ul");
-				var holdDiv = document.getElementById("display");
-			 
-			
-				var displayArray = [
-				
-				"key:..............." +  		 localStorage.keyEntry,
-				 "date:..............." + 		 localStorage.today,
-				"name:..............." + 		 localStorage.name,
-				"email:..............." +		 localStorage.eMail,
-				"city:..............." + 		 localStorage.travelCity,
-				"surveySocial:..............." + localStorage.surveySocialmedia,
-				"surveyTv:..............." + 	 localStorage.surveyTelevision,
-				"surveyradio:..............." +	 localStorage.surveyRadios,
-				"surveyradio:..............." +	 localStorage.surveyFriend,
-				"comment:..............." + 	 localStorage.comment,
-				"rating:..............." + 		 localStorage.comfort
-				
-				];
-			
-			
-				holdDiv.appendChild(newUlTag);
-				
-				for (i=0, j=displayArray.length; i<j; i++) {
-					var anLiTag = document.createElement("li");
-					anLiTag.innerHTML = displayArray[i];
-					newUlTag.appendChild(anLiTag);
-			
-			}			
-	var emptyScreen = document.getElementById("hide")
-	emptyScreen.innerHTML = "hidden=true";
-
-}
-
-
-
+ 
+ /* ====================  Event Listener ===================== */
+ 
+ 
+/*	
+	fullName.addEventListener("blur", saveData);	
+	startdate.addEventListener("blur", saveData);
+	email.addEventListener("blur", saveData);
+	city.addEventListener("blur", saveData);
+	checkBoxField.addEventListener("blur", saveData);
+	description.addEventListener("blur", saveData);
+	rating.addEventListener("blur", saveData);
+	keyValue.addEventListener("blur", saveData); 
+	
+*/
+ 
+ 
+ 
 /* ========== Clear Data  ========== */
 
 
@@ -91,3 +98,115 @@ var clearData = function() {
 	localStorage.clear(); 
 		
 }
+
+
+
+
+
+/* ========== Display Data for testing  ========== */
+
+var displayEntries = function() {
+
+var newUlTag = document.createElement("ul");
+var holdDiv = document.getElementById("display");
+
+
+
+for(i=0, j=localStorage.length; i<j; i++) {
+	var finalKey = localStorage.key(i);
+	var finalValue = localStorage.getItem(finalKey);
+	console.log (finalKey + " : " + finalValue);
+	var finalOutPut = (finalKey + " : " + finalValue);
+	var anLiTag = document.createElement("li");
+	anLiTag.innerHTML = finalOutPut;
+	newUlTag.appendChild(anLiTag);
+
+};
+ 
+holdDiv.appendChild(newUlTag);
+ 
+var emptyScreen = document.getElementById("hide")
+emptyScreen.innerHTML = "hidden=true";
+
+
+
+};
+
+
+
+/* ===================  Recover Data ================= */
+
+
+				
+
+    var recoverData = function() {
+  
+		startdate.value = localStorage.getItem("Todays Date");		
+		keyValue.innerHTML = localStorage.getItem("Unique key");
+		fullName.value = localStorage.getItem("Full Name");
+		email.value = localStorage.getItem("Email Address");
+		city.value = localStorage.getItem("Travel City");
+		checkBoxField.survey = localStorage.getItem("Survey Question"); 		
+		description.value = localStorage.getItem("Leave a Comment");
+		rating.value = localStorage.getItem("Rate your Comfort Level");
+
+
+
+
+						/* ======   Recover Check Box Data  ====== */
+				
+				
+				var x;
+				
+				x = localStorage.getItem("Survey Question");
+
+				if (x === "radios") {
+					radios.checked = "checked";				
+				};
+				
+				if (x === "socialmedia") {
+					socialmedia.checked = "checked";	
+				};
+				
+				if (x === "television") {
+					television.checked = "checked";					
+				};
+				
+				if (x === "friend") {
+					friend.checked = "checked";				
+				};
+				
+};
+
+
+
+
+/* ===================  Delete Item Function ================ */
+
+
+var deletedata = function() {
+
+	var confirmDelete = confirm("Are you sure you want to delete?");
+	if (confirmDelete) {
+		localStorage.clear();
+		localStorage.removeItem(this.key);
+		alert("Item Deleted")
+		window.location.reload();
+		}else{
+			alert("Item was not Deleted");
+		}
+	
+	}
+
+ 
+
+
+/* ======================  Call Functions and Buttons ====================  */
+
+	submitButton.addEventListener("click", saveData);
+	displayButton.addEventListener("click", displayEntries); 
+	clearButton.addEventListener("click", clearData); 
+	getInfo.addEventListener("click", recoverData)
+	removeData.addEventListener("click", deletedata)
+
+
